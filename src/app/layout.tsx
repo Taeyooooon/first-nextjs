812371217e404d1eda068537d1a9fc067869a2b8
Taeyooooon <eric568@naver.com>
2023-03-20 21:20:15 +0900

    Add: nextjs Font 태그

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,12 @@ import { Metadata } from 'next';
 import Link from 'next/link';
 import './globals.css';
 import styles from './layout.module.css';
+import { Open_Sans } from 'next/font/google';
+import { Nanum_Gothic } from 'next/font/google';
+
+// Next js 폰트
+const sans = Open_Sans({ subsets: ['latin'] });
+const gothic = Nanum_Gothic({ weight: '700', subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: '멋진 제품 사이트',
@@ -13,10 +19,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='en'>
+    <html lang='en' className={sans.className}>
       <body>
         <header className={styles.header}>
-          <h1>Demo Note App</h1>
+          <h1 className={gothic.className}>Demo Note App</h1>
           <nav className={styles.nav}>
             <Link href='/products'>Products</Link>
             <Link href='/about'>About</Link>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import Image from 'next/image';
-import { Inter } from 'next/font/google';
 import styles from './page.module.css';
 import os from 'os'; // 노드 API , 서버 컴포넌트에서는 웹 API 사용불가
 import Counter from '@/components/Counter';
 
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   console.log('안녕 - 서버');
